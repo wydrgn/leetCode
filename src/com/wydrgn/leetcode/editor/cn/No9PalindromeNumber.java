e320d71062e7b7b41acb0e57f9909e95a77f5bca
wuyunlong <583485714@qq.com>
2021-08-28 13:00:45 +0800

    第九题

---
+++ src/com/wydrgn/leetcode/editor/cn/No9PalindromeNumber.java
@@ -0,0 +1,85 @@
+//给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+//
+// 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。例如，121 是回文，而 123 不是。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：x = 121
+//输出：true
+//
+//
+// 示例 2：
+//
+//
+//输入：x = -121
+//输出：false
+//解释：从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+//
+//
+// 示例 3：
+//
+//
+//输入：x = 10
+//输出：false
+//解释：从右向左读, 为 01 。因此它不是一个回文数。
+//
+//
+// 示例 4：
+//
+//
+//输入：x = -101
+//输出：false
+//
+//
+//
+//
+// 提示：
+//
+//
+// -2³¹ <= x <= 2³¹ - 1
+//
+//
+//
+//
+// 进阶：你能不将整数转为字符串来解决这个问题吗？
+// Related Topics 数学 👍 1600 👎 0
+
+
+package com.wydrgn.leetcode.editor.cn;
+
+public class No9PalindromeNumber {
+    public static void main(String[] args) {
+        Solution solution = new No9PalindromeNumber().new Solution();
+        System.out.println(solution.isPalindrome(1234543211));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        // 别人写的更吊的东西...
+        public boolean isPalindrome(int x) {
+            String reversedStr = (new StringBuilder(x + "")).reverse().toString();
+            return (x + "").equals(reversedStr);
+        }
+
+        public boolean isPalindrome2(int x) {
+            //这次的我有经验了! letsF**k
+            String s = String.valueOf(x);
+            int left = 0;
+            int right = s.length();
+            while (left < right) {
+                if (!(s.charAt(left) == s.charAt(right - 1))) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+            // 就这?
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
