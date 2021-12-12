203fe1576ecd227c6006a2b0c71da91c03f169f7
wydrgn <583485714@qq.com>
2021-12-12 22:21:15 +0800

    32 go on

---
+++ src/com/wydrgn/leetcode/editor/cn/No32LongestValidParentheses.java
@@ -0,0 +1,59 @@
+//给你一个只包含 '(' 和 ')' 的字符串，找出最长有效（格式正确且连续）括号子串的长度。
+//
+// 
+//
+// 
+// 
+// 示例 1： 
+//
+// 
+//输入：s = "(()"
+//输出：2
+//解释：最长有效括号子串是 "()"
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = ")()())"
+//输出：4
+//解释：最长有效括号子串是 "()()"
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：s = ""
+//输出：0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= s.length <= 3 * 10⁴ 
+// s[i] 为 '(' 或 ')' 
+// 
+// 
+// 
+// Related Topics 栈 字符串 动态规划 👍 1563 👎 0
+
+
+package com.wydrgn.leetcode.editor.cn;
+
+public class No32LongestValidParentheses {
+    public static void main(String[] args) {
+        Solution solution = new No32LongestValidParentheses().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int longestValidParentheses(String s) {
+
+            return 0;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
